719957b00fb15c16f5f1d4d353442e1138de0239
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2018-08-30 22:55:27 +0800

    Added logging

--- src/graphql/mutations/mutationObjects/updateMultipleUserDetails.mutation/updateMultipleUserDetails.mutation.resolver.ts
+++ src/graphql/mutations/mutationObjects/updateMultipleUserDetails.mutation/updateMultipleUserDetails.mutation.resolver.ts
@@ -1,6 +1,6 @@
 import { GraphQLFieldResolver } from 'graphql';
 
-import { verifyToken, Models } from '@database/index';
+import { verifyToken, Models, LogAttributes } from '@database/index';
 import { FailedRequest, SuccessfulRequest } from '@graphql/types';
 
 import { UserDetails } from './updateMultipleUserDetails.mutation.types';
@@ -8,24 +8,51 @@ import { UserDetails } from './updateMultipleUserDetails.mutation.types';
 export const updateMultipleUserDetailsMutationResolver:
   GraphQLFieldResolver<void, void, UserDetails> = 
     (root, args) => {
+      let logEntry: LogAttributes = {
+        graphql_endpoint: "updateMultipleUserDetails.Mutation",
+        graphql_arguments: JSON.stringify(args)
+      };
+
       return verifyToken(args.token)
       .then(async token => {
-        if(!token) return new FailedRequest('Token is not valid');
+        if(!token) {
+          logEntry.message = "Token is not valid";
+          logEntry.request_status = false;
+
+          return await Models.Logs.create(logEntry)
+          .then(() => new FailedRequest('Token is not valid'));
+        }
 
         return Models.Users.findOne({ where: { id: token.userId } })
         .then(async user => {
-          if(!user) return new FailedRequest('User does not exist');
+          if(!user) {
+            logEntry.message = "User does not exist"
+            logEntry.request_status = false;
+
+            return await Models.Logs.create(logEntry)
+            .then(() => new FailedRequest('User does not exist'));
+          }
 
           // Update the user with the args.update
           await Object.keys(args.update).forEach(key => user[key] = args.update[key]);
 
-          return await user.save()
+          logEntry.message = "Successfully updated the user id=" + user.id;
+          logEntry.request_status = true;
+          logEntry.user = user.id;
+
+          return await Models.Logs.create(logEntry)
+          .then(async () => await user.save())
           .then(async () => await token.destroy())
           .then(async () => new SuccessfulRequest({updatedDetails: args.update}));
         });
       })
-      .catch(err => {
+      .catch(async err => {
         console.log(err);
-        return new FailedRequest('Error occurred during the update of the user, please try again');
+
+        logEntry.message = JSON.stringify(err);
+        logEntry.request_status = false;
+
+        return await Models.Logs.create(logEntry)
+        .then(() => new FailedRequest('Error occurred during the update of the user, please try again'));
       });
     };
