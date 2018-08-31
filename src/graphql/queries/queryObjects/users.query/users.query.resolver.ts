d6aa98c6a4affdf9d61e4ac68f7080a3bcd2ceed
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2018-08-31 10:47:53 +0800

    Added logging to users.query

--- src/graphql/queries/queryObjects/users.query/users.query.resolver.ts
+++ src/graphql/queries/queryObjects/users.query/users.query.resolver.ts
@@ -1,8 +1,26 @@
 import { GraphQLFieldResolver } from 'graphql';
 
-import { UserAttributes, Models } from '@database/index';
+import { UserAttributes, Models, LogAttributes, UserTypes } from '@database/index';
 
 export const userQueryResolver: GraphQLFieldResolver<UserAttributes, void> = 
   (source, args) => {
-    return Models.Users.findAll({ where: args });
+    let logEntry: LogAttributes = {
+      graphql_endpoint: 'users.query',
+      graphql_arguments: JSON.stringify(args)
+    };
+
+    logEntry.message = "User Query Successful";
+    logEntry.request_status = true;
+
+    return Models.Users.findAll({ where: args })
+    .then(async (users) => { await Models.Logs.create(logEntry); return users; })
+    .catch(async err => {
+      console.log(err);
+
+      logEntry.message = JSON.stringify(err);
+      logEntry.request_status = true;
+
+      return await Models.Logs.create(logEntry)
+      .then(() => []);
+    })
   }
