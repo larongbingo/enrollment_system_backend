35787a99507b2ccde21fc144bfc2228d232ea900
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2018-09-04 16:07:45 +0800

    Removed deprecated variable

--- src/graphql/mutations/mutationObjects/createUser.mutation/createUser.mutation.ts
+++ src/graphql/mutations/mutationObjects/createUser.mutation/createUser.mutation.ts
@@ -4,41 +4,6 @@ import GraphQLJSON from 'graphql-type-json';
 import { createUserMutationResolver } from './createUser.mutation.resolver';
 import { RequiredValidFieldsType } from './createUser.mutation.types';
 
-/**
- * @deprecated args has been aggregated as a single object
- */
-export const createUserMutation: GraphQLFieldConfig<void, void, any> = {
-  description: 'Creates a new user using the information required by the mutation',
-  type: GraphQLJSON,
-  args: {
-    firstName: {
-      type: new GraphQLNonNull(GraphQLString),
-      description: 'The firstname of the user'
-    },
-    middleName: {
-      type: new GraphQLNonNull(GraphQLString),
-      description: 'The middlename of the user'
-    },
-    lastName: {
-      type: new GraphQLNonNull(GraphQLString),
-      description: 'The lastname of the user'
-    },
-    address: {
-      type: GraphQLString,
-      description: 'The address of the user'
-    },
-    username: {
-      type: new GraphQLNonNull(GraphQLString),
-      description: 'The username of the user'
-    },
-    password: {
-      type: new GraphQLNonNull(GraphQLString),
-      description: 'The password of the user'
-    }
-  },
-  resolve: createUserMutationResolver
-};
-
 export const updatedCreateUserMutation: GraphQLFieldConfig<void, void, any> = {
   description: 'Creates a new user using the information required by the mutation',
   type: GraphQLJSON,
