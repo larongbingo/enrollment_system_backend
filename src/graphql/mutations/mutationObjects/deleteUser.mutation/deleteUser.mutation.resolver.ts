7771b2937ed594549da888b32988595342b52680
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2018-09-08 22:23:53 +0800

    Added logging to deleteUser.mutation

--- src/graphql/mutations/mutationObjects/deleteUser.mutation/deleteUser.mutation.resolver.ts
+++ src/graphql/mutations/mutationObjects/deleteUser.mutation/deleteUser.mutation.resolver.ts
@@ -2,26 +2,32 @@ import { GraphQLFieldResolver } from 'graphql';
 
 import { verifyToken, Models } from '@database/index';
 import { SuccessfulRequest, FailedRequest } from '@graphql/types';
+import { SuccessfulRequestLogging, FailedRequestLogging } from '@graphql/util';
 
 import { TokenStorage } from './deleteUser.mutation.types';
 
 export const deleteUserMutationResolver: GraphQLFieldResolver<void, void, TokenStorage> =
 (source, args) => {
+  const ENDPOINT_NAME = 'deleteUser.mutation';
+
   return verifyToken(args.token)
   .then(async token => {
     if(!token) {
-      return new FailedRequest('Token is not valid');
+      return await FailedRequestLogging(args, ENDPOINT_NAME, 'Invalid token')
+      .then(() => new FailedRequest('Token is not valid'));
     }
 
     return await Models.Users.findOne({ where: { id: token.userId } })
     .then(async user => {
       if(!user) {
-        return new FailedRequest('User does not exist');
+        return await FailedRequestLogging(args, ENDPOINT_NAME, 'User does not exist')
+        .then(() => new FailedRequest('User does not exist'));
       }
 
       await user.destroy();
 
-      return new SuccessfulRequest({ deletedUser: { id: user.id } });
+      return SuccessfulRequestLogging(args, ENDPOINT_NAME, 'Deleted id=' + user.id)
+      .then(() => new SuccessfulRequest({ deletedUser: { id: user.id } }));
     })
   })
 }
