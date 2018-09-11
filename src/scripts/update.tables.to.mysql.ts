1374c1c75fb357db36bfeb992c11f0aa96e0baa5
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2018-09-11 12:50:01 +0800

    Check if NODE_ENV is not production

--- src/scripts/update.tables.to.mysql.ts
+++ src/scripts/update.tables.to.mysql.ts
@@ -4,5 +4,10 @@ import { sequelize } from '@database/index';
 
 console.log('Updating MySQL Client with the current Models');
 
+if(process.env.NODE_ENV === 'production') {
+  console.log('Environment is in "production", this WILL DELETE existing user data');
+  process.exit(1);
+}
+
 sequelize.sync({ force: true })
 .then(() => process.exit(0));
