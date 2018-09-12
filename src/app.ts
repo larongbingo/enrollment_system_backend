4c639a75c1618412c8217a473e6c1018ee8a9eeb
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2018-09-12 22:46:43 +0800

    Added CSRF token request route

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,11 @@ import express from 'express';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import GraphqlHTTP from 'express-graphql';
+import RandomString from 'randomstring';
 
 import { Schema } from '@graphql/schema';
 import { Create404, ErrorHandler } from './middlewares';
+import { Models, TokenTypes, TokenAttributes } from '@database/index';
 
 const app = express();
 
@@ -23,6 +25,18 @@ app.use('/api', GraphqlHTTP({
   schema: Schema,
   graphiql: false
 }));
+
+app.get('/token', function(req, res) {
+  let randomString = RandomString.generate(150);
+
+  let tokenModelInstance: TokenAttributes = {
+    token_type: TokenTypes.Request,
+    token: randomString
+  };
+
+  Models.Tokens.create(tokenModelInstance);
+  res.send(randomString);
+});
 
 if(process.env.NODE_ENV === 'testing') {
   app.use('/graphiql', GraphqlHTTP({
